d1ec8edded2264a4a7c55a1db5ae009af9b718cb
shubhajyoti.paul <shubhajyoti.paul@cgi.com>
2019-09-04 01:21:56 +0530

    ver 1.0.1

---
+++ Isogram.Tests/isogramtest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using isogram_csharp_problem;
+using Xunit;
+
+namespace Isogram.Tests
+{
+    public class isogramtest
+    {
+        
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("isogram", true)]
+        [InlineData("eleven", false)]
+        [InlineData("subdermatoglyphic", true)]
+        [InlineData("Alphabet", true)]
+        [InlineData("thumbscrew-japingly", true)]
+        [InlineData("six-year-old", true)]
+        [InlineData("n a m e", true)]
+        [InlineData("QWERTYUIOPASDFGHJKLZXCVBNM - - -qwertyuiop  asdfghjkl zxcvbnm", true)]
+
+        [InlineData("QWERTYUIOPASDFGHJKLZXCVBNM - - -qwerrtyuiop  asdfghjkl zxcvbnm", false)]
+        public void CheckIsogram(string x,bool y)
+        {
+            Assert.Equal(Program.IsogramCheck(x), y);
+        }
+    }
+}
---
+++ isogram-csharp-problem/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace isogram_csharp_problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsogramCheck(string x)
+        {
+            //return a + b;
+            //return false;
+
+            if (x == "") return true;
+
+            int[] place = new int[200];
+            for(int i=0;i<x.Length;i+=1)
+            {
+                place[(int)(x[i])] += 1;
+            }
+            bool flag = true;
+            for (int i = 65; i <= 90; i += 1)
+            {
+                if (place[i] > 1)
+                {
+                    flag = false; break;
+                }
+            }
+            for (int i = 97; i <= 122; i += 1)
+            {
+                if (place[i] > 1)
+                {
+                    flag = false; break;
+                }
+            }
+            return flag;
+
+        }
+
+
+    }
+}
